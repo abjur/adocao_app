93cb2835ad9f2cdd12f4a22a3eeff9353b612e2b
jtrecenti <julio.trecenti@gmail.com>
2021-05-25 00:37:09 -0300

    tapa no código e validate need

--- shiny/app.R
+++ shiny/app.R
@@ -9,7 +9,7 @@ n_criancas <- nrow(criancas)
 
 pais <- readr::read_rds("data/pais.rds") %>%
   dplyr::slice_sample(n = 10)
-n_pais = nrow(pais)
+n_pais <- nrow(pais)
 
 tempos_entre_chegadas <- "data/tempos_entre_chegadas.rds" %>%
   readr::read_rds() %>%
@@ -27,38 +27,45 @@ escolhas_raca <-  c(
 )
 
 # ui ---------------------------------------------------------------------------
-ui <-
-  fluidPage(titlePanel("Selecione o perfil da criança Desejada"),
-            fluidRow(column(
-              3,
-              wellPanel(
-                h4("Selecione o perfil desejado"),
-                sliderInput(
-                  inputId = "idade",
-                  label = "Faixa etária",
-                  0,
-                  18,
-                  value = c(1, 7)
-                ),
-                # selecionar sexo
-                checkboxGroupInput("sexo", "Sexo", escolhas_sexo, selected = escolhas_sexo),
-                # selecionar raça
-                checkboxGroupInput("cor", "Raça:", escolhas_raca, selected = escolhas_raca),
-                actionButton("action", label = "Iniciar"),
-              )
-            ),
-            column(9, wellPanel(
-              span(
-                "Tempo médio de espera na fila para adotar uma criança com esse perfil (em dias):",
-                tableOutput("t_adocao_m") %>% withSpinner()
-              ),
+ui <- fluidPage(
+  titlePanel("Selecione o perfil da criança Desejada"),
+  fluidRow(
+    column(
+      width = 3,
+      wellPanel(
+        h4("Selecione o perfil desejado"),
+        sliderInput(
+          inputId = "idade",
+          label = "Faixa etária",
+          0,
+          18,
+          value = c(1, 7)
+        ),
+        # selecionar sexo
+        checkboxGroupInput("sexo", "Sexo", escolhas_sexo, selected = escolhas_sexo),
+        # selecionar raça
+        checkboxGroupInput("cor", "Raça:", escolhas_raca, selected = escolhas_raca),
+        actionButton("action", label = "Iniciar"),
+      )
+    ),
+    column(
+      width = 9,
+      wellPanel(
+        span(
+          "Tempo médio de espera na fila para adotar uma criança com esse perfil (em dias):",
+          tableOutput("t_adocao_m") %>% withSpinner()
+        ),
+        plotOutput("plot2")
+      )
+    )
+  )
+)
 
-              plotOutput("plot2")
-            ))))
 
 # Server -----------------------------------------------------------------------
 server <- function(input, output, session) {
-  perfil_pai = reactive({
+
+  perfil_pai <- reactive({
     data.frame(
       idade_minima = input$idade[1],
       idade_maxima = input$idade[2],
@@ -71,16 +78,24 @@ server <- function(input, output, session) {
       cor_indigena = "Indígena" %in% input$cor
     )
   })
-  tempos_adocao = eventReactive(input$action, {
-    tempo_adocao_m(perfil_pai(),
-                   criancas,
-                   pais,
-                   tempos_entre_chegadas,
-                   n_sim = 100)
+
+  tempos_adocao <- eventReactive(input$action, {
+    tempo_adocao_m(
+      perfil_pai(),
+      criancas,
+      pais,
+      tempos_entre_chegadas,
+      n_sim = 100
+    )
   })
 
   output$t_adocao_m <- renderTable({
     tempos <- tempos_adocao()
+    shiny::validate(shiny::need(
+      all(is.finite(tempos)),
+      "Tempo estimado foi maior do que 20 anos."
+    ))
+
     tibble::tibble(
       media = mean(tempos),
       "1º quartil" = quantile(tempos, .25),
@@ -90,12 +105,13 @@ server <- function(input, output, session) {
   })
 
   output$plot2 <- renderPlot({
-    ggplot(tibble::tibble(tempos = tempos_adocao()), aes(x = tempos)) +
-      geom_histogram(colour = "transparent",
-                     fill = "#414487") +
+    tempos <- tempos_adocao()
+    shiny::validate(shiny::need(all(is.finite(tempos)), ""))
+
+    ggplot(tibble::tibble(tempos = tempos), aes(x = tempos)) +
+      geom_histogram(colour = "transparent", fill = "#414487", bins = 30) +
       labs(x = "Tempos de adoção") +
       theme_minimal(12)
-
   })
 
 }
